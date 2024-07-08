a7616d6cc826f344bb2d64ea6b7638f59f1b050b
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-07-07 22:18:21 -0400

    Creando servicios de usuarios

---
+++ UserBlazorApp.API/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersBlazorApp.API.Context;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Services;
+
+public class UserService(UsersDbContext context) : IAPIService<AspNetUsers>
+{
+    public async Task<List<AspNetUsers>> GetAll()
+    {
+        return await context.AspNetUsers
+            .Include(u => u.Role)
+            .ThenInclude(r => r.AspNetRoleClaims)
+            .ToListAsync();
+    }
+
+    public async Task<AspNetUsers> Get(int id)
+    {
+        return await context.AspNetUsers.FirstAsync(u => u.Id == id);
+    }
+
+    public async Task<AspNetUsers> Add(AspNetUsers user)
+    {
+        if (!await Existe(user.Id))
+        {
+            context.AspNetUsers.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+        else
+        {
+            await Update(user);
+            return await Get(user.Id);
+        }
+    }
+
+    public async Task<bool> Update(AspNetUsers user)
+    {
+        context.Entry(user).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        var usuario = await context.AspNetUsers.FirstOrDefaultAsync(u => u.Id == id);
+        if (usuario == null)
+        {
+            return false;
+        }
+        context.AspNetUsers.Remove(usuario);
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Existe(int id)
+    {
+        return await context.AspNetUsers.AnyAsync(u => u.Id == id);
+    }
+}
---
+++ UserBlazorApp.UI/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+public class UserClienService(HttpClient httpClient) : IClientService<AspNetUsers>
+{
+    public async Task<List<AspNetUsers>> GetAll()
+    {
+        return await httpClient.GetFromJsonAsync<List<AspNetUsers>>("api/AspNetUsers");
+    }
+
+    public async Task<AspNetUsers> Get(int id)
+    {
+        return await httpClient.GetFromJsonAsync<AspNetUsers>("api/AspNetUsers");
+    }
+
+    public async Task<AspNetUsers> Add(AspNetUsers property)
+    {
+        var response = await httpClient.PostAsJsonAsync("api/AspNetUsers", property);
+        return await response.Content.ReadFromJsonAsync<AspNetUsers>();
+    }
+
+    public async Task<bool> Update(AspNetUsers property)
+    {
+        var response = await httpClient.PutAsJsonAsync($"api/AspNetUsers/{property.Id}", property);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        var response = await httpClient.DeleteAsync($"api/AspNetUsers/{id}");
+        return response.IsSuccessStatusCode;
+    }
+}
