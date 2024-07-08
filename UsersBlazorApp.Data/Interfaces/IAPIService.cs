71758c360d7ca6a247a2074a87a7f9f351bdd668
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-07-07 21:57:22 -0400

    Creando interfaces

---
+++ UsersBlazorApp.Data/Interfaces/IAPIService.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersBlazorApp.Data.Interfaces;
+
+public interface IAPIService <T>
+{
+    Task<List<T>> GetAll();
+    Task<T> Get(int id);
+    Task<T> Add(T user);
+    Task<bool> Update(T user);
+    Task<bool> Delete(int id);
+}
