4a7d7d279fcdcc62306f2b3c18a336552f3dda27
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-07-08 17:46:30 -0400

    Agregando interfaces de claims

---
+++ UsersBlazorApp.Data/Interfaces/IRoleClaimsService.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersBlazorApp.Data.Interfaces;
+
+public interface IRoleClaimsService<T>
+{
+    Task<List<T>> GetAll();
+    Task<T> Get(int id);
+    Task<T> Add(T role);
+    Task<bool> Update(T role);
+    Task<bool> Delete(int id);
+}
