eb47869e9a91fb72136e8efe0d4f6a5e3dad2d21
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-07-08 17:33:20 -0400

    Agregando controllers de claims

---
+++ UserBlazorApp.API/Controllers/RoleClaimsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UsersBlazorApp.API.Context;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleClaimsController : ControllerBase
+    {
+        private readonly UsersDbContext _context;
+
+        public RoleClaimsController(UsersDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RoleClaims
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AspNetRoleClaims>>> GetAspNetRoleClaims()
+        {
+            return await _context.AspNetRoleClaims.ToListAsync();
+        }
+
+        // GET: api/RoleClaims/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AspNetRoleClaims>> GetAspNetRoleClaims(int id)
+        {
+            var aspNetRoleClaims = await _context.AspNetRoleClaims.FindAsync(id);
+
+            if (aspNetRoleClaims == null)
+            {
+                return NotFound();
+            }
+
+            return aspNetRoleClaims;
+        }
+
+        // PUT: api/RoleClaims/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAspNetRoleClaims(int id, AspNetRoleClaims aspNetRoleClaims)
+        {
+            if (id != aspNetRoleClaims.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aspNetRoleClaims).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AspNetRoleClaimsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RoleClaims
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AspNetRoleClaims>> PostAspNetRoleClaims(AspNetRoleClaims aspNetRoleClaims)
+        {
+            _context.AspNetRoleClaims.Add(aspNetRoleClaims);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAspNetRoleClaims", new { id = aspNetRoleClaims.Id }, aspNetRoleClaims);
+        }
+
+        // DELETE: api/RoleClaims/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAspNetRoleClaims(int id)
+        {
+            var aspNetRoleClaims = await _context.AspNetRoleClaims.FindAsync(id);
+            if (aspNetRoleClaims == null)
+            {
+                return NotFound();
+            }
+
+            _context.AspNetRoleClaims.Remove(aspNetRoleClaims);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AspNetRoleClaimsExists(int id)
+        {
+            return _context.AspNetRoleClaims.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ UserBlazorApp.API/Controllers/UserClaimsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UsersBlazorApp.API.Context;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserClaimsController : ControllerBase
+    {
+        private readonly UsersDbContext _context;
+
+        public UserClaimsController(UsersDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserClaims
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AspNetUserClaims>>> GetAspNetUserClaims()
+        {
+            return await _context.AspNetUserClaims.ToListAsync();
+        }
+
+        // GET: api/UserClaims/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AspNetUserClaims>> GetAspNetUserClaims(int id)
+        {
+            var aspNetUserClaims = await _context.AspNetUserClaims.FindAsync(id);
+
+            if (aspNetUserClaims == null)
+            {
+                return NotFound();
+            }
+
+            return aspNetUserClaims;
+        }
+
+        // PUT: api/UserClaims/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAspNetUserClaims(int id, AspNetUserClaims aspNetUserClaims)
+        {
+            if (id != aspNetUserClaims.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aspNetUserClaims).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AspNetUserClaimsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserClaims
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AspNetUserClaims>> PostAspNetUserClaims(AspNetUserClaims aspNetUserClaims)
+        {
+            _context.AspNetUserClaims.Add(aspNetUserClaims);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAspNetUserClaims", new { id = aspNetUserClaims.Id }, aspNetUserClaims);
+        }
+
+        // DELETE: api/UserClaims/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAspNetUserClaims(int id)
+        {
+            var aspNetUserClaims = await _context.AspNetUserClaims.FindAsync(id);
+            if (aspNetUserClaims == null)
+            {
+                return NotFound();
+            }
+
+            _context.AspNetUserClaims.Remove(aspNetUserClaims);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AspNetUserClaimsExists(int id)
+        {
+            return _context.AspNetUserClaims.Any(e => e.Id == id);
+        }
+    }
+}
