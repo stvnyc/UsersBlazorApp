3be50be94424f1a379e335d1404300b3daacba38
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-07-08 17:28:28 -0400

    Agregando Interfaces con los nombres correctos

--- UsersBlazorApp.Data/Interfaces/IRoleService.cs
+++ UsersBlazorApp.Data/Interfaces/IRoleService.cs
@@ -1,10 +1,10 @@
 ﻿namespace UsersBlazorApp.Data.Interfaces;
 
-public interface IClientService <T>
+public interface IRoleService <T>
 {
     Task<List<T>> GetAll();
     Task<T> Get(int id);
-    Task<T> Add(T user);
-    Task<bool> Update(T user);
+    Task<T> Add(T role);
+    Task<bool> Update(T role);
     Task<bool> Delete(int id);
 }
